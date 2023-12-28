7cc7caf5eb0af942f1227a27d6f254e6a2d12001
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-12-28 15:54:46 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,3 @@
 fn main(){
-    println!("Hello Ji kese ho saare");
+    println!("Hello Ji kese ho saare");             // everything in rust is wrapped inside of the main function  working as the macro here 
 }
