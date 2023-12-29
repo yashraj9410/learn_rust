9c7d586dac68c595b16aeb70e6b9b117e8f02dd6
Yash Raj <yashraj941015@gmail.com>
2023-12-29 19:33:57 +0530

    rotate array by k

---
+++ rotateArray.rs
@@ -0,0 +1,17 @@
+fn reverse(arr: &mut [i32], i:i32 , j:i32){
+    while(i<j){
+        let mut temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+fn main(){
+    let mut arr:[i32;5] = [1,2,3,4,5]   // variables and array are immutabls by default but to make them mutable we can use the mut keyword
+    reverse(&mut arr,0,4);
+    reverse(&mut arr, 3, 4);
+    reverse(&mut arr, 0 , 3);
+}
